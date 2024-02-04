81da2120c9a12a0e8c44573dced05431a4221556
morisum <moris@r.sannet.ne.jp>
2024-02-04 17:32:56 +0900

    dekita1

--- ToDo1/ContentView.swift
+++ ToDo1/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var taskData = [
+        (title: "ジョギングする", completed: false),
+        (title: "お花に水をやる", completed: false),
+        (title: "部屋の掃除をする", completed: false),
+        (title: "本を読む", completed: false)]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(0..<taskData.count, id: \.self) { index in
+                Button {
+                    taskData[index].completed.toggle()
+                } label: {
+                    HStack {
+                        Image(systemName: taskData[index].completed ? "checkmark.circle.fill" : "circle")
+                        Text(taskData[index].title)
+                    }
+                }
+                .foregroundColor(.primary)
+//    カーソルを行末で　command + D でコピー
+            }
+            .navigationTitle("ToDoリスト")
         }
-        .padding()
     }
 }
 
